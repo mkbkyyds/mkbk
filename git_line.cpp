1f4beb28f885beeae9694c7a5a80685f1299a350
mkbk <2943823931@qq.com>
2021-07-20 08:48:00 +0800

    新增函数

--- git_line.cpp
+++ git_line.cpp
@@ -1,7 +1,13 @@
 #include<iostream>
+#include<cstring>
 using namespace std;
 int main()
 {
 	cout<<"安妮嫩叠"<<endl;
 	return 0;
+}
+string git_string()
+{
+	string str = "安妮嫩叠"<<endl;
+	return string;
 }
